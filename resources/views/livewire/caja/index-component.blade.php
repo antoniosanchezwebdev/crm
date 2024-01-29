66883ba6c8d5e08a70685f9a2d36c3e3ed6bbffc
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-01-29 13:00:25 +0100

    commit estetico

--- resources/views/livewire/caja/index-component.blade.php
+++ resources/views/livewire/caja/index-component.blade.php
@@ -1,8 +1,8 @@
 <div id="contenedorCaja">
     @if (isset($movimientos))
-        <div>
-            <input type="date" class="form-control" wire:model="fechaInicio">
-            <input type="date" class="form-control" wire:model="fechaFin">
+        <div style="display: flex; align-items: center; justify-content: space-around; gap: 10px;">
+            <input type="date" wire:model="fechaInicio">
+            <input type="date" wire:model="fechaFin">
             <button type="button" class="btn btn-primary boton-producto" wire:click="calcularTotal">Calcular</button>
             <h4>Total Movimientos: €{{ $totalMovimientos }}</h4>
         </div>
