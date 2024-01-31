73d72ca511371e546b5f0f97ddb3c7ad2d9181b6
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-01-31 13:12:55 +0100

    test 2

--- resources/views/livewire/trabajadores/index-component.blade.php
+++ resources/views/livewire/trabajadores/index-component.blade.php
@@ -37,9 +37,9 @@ $user = Auth::user();
                                     @endphp
                                     <div class="progress">
                                         <div class="progress-bar" role="progressbar" 
-                                            style="width:100%;" 
+                                            style="width:{{ $productividadPorTrabajador[$trabajador->id] }}%;" 
                                             aria-valuenow="{{ $productividadPorTrabajador[$trabajador->id] }}" 
-                                            aria-valuemin="0" aria-valuemax="100">
+                                            aria-valuemin="1" aria-valuemax="100">
                                             {{ number_format($productividadPorTrabajador[$trabajador->id], 2) }}%
                                         </div>
                                      </div>
