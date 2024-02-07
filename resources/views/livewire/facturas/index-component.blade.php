0875c38bb2fb5d64d509137ba5aaa881229a2ef9
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-02-07 14:05:13 +0100

    test 2

--- resources/views/livewire/facturas/index-component.blade.php
+++ resources/views/livewire/facturas/index-component.blade.php
@@ -89,3 +89,14 @@
 </div>
 </div>
 </div>
+@section('script')
+<script>
+
+document.addEventListener('DOMContentLoaded', function() {
+  var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+  var dropdownList = dropdownElementList.map(function(dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl);
+  });
+});
+</script>
+@endsection
